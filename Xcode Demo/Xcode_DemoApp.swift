b481dea0a13799d87873d756b2e4dd929f9ffca0
Rubin <rubinradhakrishnan@gmail.com>
2024-08-20 22:23:54 +0530

    Initial Commit

---
+++ Xcode Demo/Xcode_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_DemoApp.swift
+//  Xcode Demo
+//
+//  Created by Rubin on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Xcode DemoTests/Xcode_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_DemoTests.swift
+//  Xcode DemoTests
+//
+//  Created by Rubin on 20/08/24.
+//
+
+import Testing
+@testable import Xcode_Demo
+
+struct Xcode_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
